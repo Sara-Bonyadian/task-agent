ee20c696e24b1d7678b0ef8b57cff4ffc00a2351
Sara Bonyadian <sara.bonyadian@gmail.com>
2025-08-28 16:55:17 -0400

    Add /tasks GET endpoint

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app=FastAPI()
 @app.get("/")
 def read_root():
     return{"message":"uvicornTask Agent Backend is runing"}
+
+@app.get("/tasks")
+def get_tasks():
+    return[
+        {"id":1,"title":"Buy groceries","done":False},
+        {"id":2,"title":"Finish homework","done":True},
+        {"id":3,"title":"Call mom","done":False},
+        ]
