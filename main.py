cf4c2929b4ac984b6eee23d97e520c68c200379c
Sara Bonyadian <sara.bonyadian@gmail.com>
2025-08-28 19:52:42 -0400

    Add Get and Post

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 app=FastAPI()
+tasks = [
+    {"id": 1, "title": "Buy groceries", "done": False},
+    {"id": 2, "title": "Finish homework", "done": True},
+]
+
+class Task(BaseModel):
+    title:str
+    done:bool=False
 
 @app.get("/")
 def read_root():
@@ -8,8 +16,10 @@ def read_root():
 
 @app.get("/tasks")
 def get_tasks():
-    return[
-        {"id":1,"title":"Buy groceries","done":False},
-        {"id":2,"title":"Finish homework","done":True},
-        {"id":3,"title":"Call mom","done":False},
-        ]
+    return tasks
+
+@app.post("/tasks")
+def create_task(task:Task):
+    new_task={"id":len(tasks)+1,"title": task.title, "done":task.done}
+    tasks.append(new_task)
+    return new_task
